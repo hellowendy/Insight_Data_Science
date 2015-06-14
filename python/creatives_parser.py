bb7a79c5825c827124b900ee11766d06fdd98902
Wenjun Shao <hellowendy01@gmail.com>
2015-06-14 16:26:37 -0700

    Look at specific apps

---
+++ python/creatives_parser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 13:35:35 2015
+
+@author: wenjun
+"""
+
+#import sys
+#import os
+import re
+
+def main():
+    header = 'cr\tname\tad_type'
+    print header
+    f = open('/home/wenjun/Documents/2015_Insight_SV/zenit/data/zenit_creatives.txt', 'r')
+    for line in f:
+        m = re.match('^id: (\d+), name: ([\w_ \#]+), format: .+, ad_type: ([\w_]+), categories:',line)
+        if m:
+            print "%s\t%s\t%s" % (m.group(1), m.group(2), m.group(3))
+    f.close()
+
+if __name__ == '__main__':
+    main()
