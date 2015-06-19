c786f7bb2fcd9bf5595b98098a4be3eccc5c061e
Wenjun Shao <hellowendy01@gmail.com>
2015-06-19 09:37:40 -0700

    did more analysis in notebook

---
+++ python/ads_app_count.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 17
+
+@author: wenjun
+"""
+
+from elasticsearch import Elasticsearch
+import eslogin
+
+import pandas as pd
+
+def main():
+    es = Elasticsearch([{'host': eslogin.host, 'port': eslogin.port}],
+                        http_auth=(eslogin.user,eslogin.password))
+    
+    columns = range(150)
+    index = range(360)
+    df = pd.DataFrame(index=index, columns=columns)
+    df = df.fillna(0)
+    
+    for col in columns:
+        for ind in index:
+            df.loc[ind,col] = es.count(index="events-2015.05.*", body={'query': {'bool': {'must':[{'match': { 'ai' : col }}, {'match': { 'cr' : ind }}, {'match': { 'et' : 'AD_SHOW' }}],'must_not':[{'match': { 'fr' : 'true' }}]}}})['count']
+
+    df.to_csv("../data/zenit_ad_show_5_2015.tab",sep='\t')
+
+if __name__ == '__main__':
+    main()
